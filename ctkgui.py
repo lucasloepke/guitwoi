8c27bee3bd8ee59ab5751cb73585bd70c3276196
lucasloepke <wavyknight72@gmail.com>
2024-07-23 13:51:30 -0400

    update

--- ctkgui.py
+++ ctkgui.py
@@ -3,15 +3,19 @@ from tkinter.filedialog import askopenfilename
 import sys
 
 def qbutton():
+    m.title('Goodbye!')
+    button.configure(text="  Goodbye!  ")
+    frame.update_idletasks()
+    frame.after(350)
     sys.exit()
 
 def start_progress():
-    progress.place(x=100, y=150)
+    progress.place(x=185, y=210)
     if progress.get() < 1:
         progress.set(progress.get() + .02)
         frame.after(3, start_progress)
     else:
-        text.pack(side='bottom', pady=40)
+        text.pack(side='bottom', pady=10)
         frame.update_idletasks()
         frame.after(1200, text.configure(text='Program Ending...'))
         frame.update_idletasks()
@@ -21,8 +25,9 @@ def selact():
     global act
     actions = askopenfilename()
     if actions:
-        actbut.configure(fg_color='green', text='Actions Selected ✅', 
-                         hover_color='dark green', state="disabled")
+        actbut.configure(fg_color='green', text='Actions ✅', 
+                         hover_color='dark green', state="disabled",
+                         font=ctk.CTkFont(family='72', weight="normal"))
         act = True
         try:
             if sta:
@@ -33,8 +38,9 @@ def selsta():
     global sta
     statements = askopenfilename()
     if statements:
-        stabut.configure(fg_color='green', text='Statements Selected ✅', 
-                         hover_color='dark green', state="disabled")
+        stabut.configure(fg_color='forest green', text='Statements ✅', 
+                         hover_color='dark green', state="disabled",
+                         font=ctk.CTkFont(family='72', weight="normal"))
         sta = True
         try:
             if act:
@@ -46,6 +52,7 @@ def switch_event():
         stabut.configure(fg_color='#1F6AA5', hover_color='#144870')
         actbut.configure(fg_color='#1F6AA5', hover_color='#144870')
         start_button.configure(fg_color='#1F6AA5', hover_color='#144870')
+        progress.configure(progress_color='blue violet')
         try:
             if sta: stabut.configure(fg_color='green', hover_color='dark green')
         except: pass
@@ -58,6 +65,7 @@ def switch_event():
         stabut.configure(fg_color='SpringGreen3', hover_color='SpringGreen4')
         actbut.configure(fg_color='SpringGreen3', hover_color='SpringGreen4')
         start_button.configure(fg_color='SeaGreen3', hover_color='SeaGreen4')
+        progress.configure(progress_color='goldenrod1')
         try:
             if sta: stabut.configure(fg_color='green', hover_color='dark green')
         except: pass
@@ -74,33 +82,41 @@ m.geometry("400x300")
 m.title("Expensive Statements Summary")
 switch_var = ctk.StringVar(value="off")
 
-# Frame
+# Frames
 frame = ctk.CTkFrame(master=m, width=380, height=280, border_color='red')
-frame.place(x=10, y=10)
-frame.pack_propagate(0)  # Prevent frame from resizing 
+sframe = ctk.CTkFrame(master=m, width=160, height=260, border_color='red', fg_color='gray25')
+ssframe = ctk.CTkFrame(master=m, width=140, height=40, border_color='gold', fg_color='gray45')
+ssframe.place(x=30, y=230); ssframe.pack_propagate(0)
+sframe.place(x=20, y=20); sframe.pack_propagate(0)
+frame.place(x=10, y=10); frame.pack_propagate(0)  # Prevent frame from resizing 
 
 # Widgets
 actions = ''; statements = ''
-actbut = ctk.CTkButton(frame, text="Select Actions", command=selact)
-stabut = ctk.CTkButton(frame, text="Select Statements", command=selsta)
-button = ctk.CTkButton(frame, text="End Process", command=qbutton, fg_color='gray', hover_color='dim gray')
-switch = ctk.CTkSwitch(frame, text="", command=switch_event, variable=switch_var, offvalue="off", onvalue="on", progress_color='SeaGreen3')
+actbut = ctk.CTkButton(sframe, text="Select Actions", command=selact, 
+                       font=ctk.CTkFont(family='72', weight="bold"))
+stabut = ctk.CTkButton(sframe, text="Select Statements", command=selsta, 
+                       font=ctk.CTkFont(family='72', weight="bold"))
+button = ctk.CTkButton(frame, text="End Process", command=qbutton, fg_color='gray', 
+                       hover_color='dim gray', font=ctk.CTkFont(family='72', weight="bold"))
+switch = ctk.CTkSwitch(frame, text="", command=switch_event, variable=switch_var, 
+                       offvalue="off", onvalue="on", progress_color='SeaGreen3')
 
 # Progress bar
-progress = ctk.CTkProgressBar(frame, width=180, progress_color='SeaGreen1')
-start_button = ctk.CTkButton(frame, text="Create Summary", command=start_progress, state='disabled')
-text = ctk.CTkLabel(frame, text='Summary Created!')
+progress = ctk.CTkProgressBar(frame, width=180, progress_color='blue violet', height=20)
+start_button = ctk.CTkButton(sframe, text="Create Summary", command=start_progress, 
+                             state='disabled', font=ctk.CTkFont(family='72'))
+text = ctk.CTkLabel(ssframe, text='Summary Created!')
 
 # Window Packing
 m.minsize(400, 300)
 m.maxsize(400, 300)
 
-stabut.place(x=20,y=20)
-actbut.place(x=220,y=20)
-start_button.place(x=120,y=100)
+stabut.place(x=10,y=10)
+actbut.place(x=10,y=50)
+start_button.place(x=10,y=90)
 progress.set(0); progress.pack_forget()
 text.pack_forget()
 switch.place(x=335, y=245)
-button.pack(side='bottom', pady=10)
+button.place(x=185, y=242)
 
 m.mainloop()
